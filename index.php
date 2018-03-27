e1555d8fcd231c96b902df87d50d008c59acbaef
Joe <jmessare46@gmail.com>
2018-03-27 14:54:09 -0400

    Created first shell of the homepage for website

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Kung Fu site home page.
+ *
+ * @version 3/27/18
+ * @author Joseph Messare
+ */
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+</head>
+<body>
+
+<!-- Navigation list shown at the top of the page -->
+<div id="nav">
+    <ul id="navlist">
+        <li class="active">Home</li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</div>
+
+<h1 id="title">Kung Fu School</h1>
+
+<p>
+    Home page content would go here.
+</p>
+</body>
+</html>
