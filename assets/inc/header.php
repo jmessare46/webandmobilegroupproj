7d40ee7b0333655d9ece08b902abf14ade46fcd1
Joe <jmessare46@gmail.com>
2018-03-29 12:54:44 -0400

    Added a heading abstraction file to avoid repeat code

---
+++ assets/inc/header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joseph Messare
+ * Date: 3/29/18
+ * Time: 12:23 PM
+ */
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title><?php echo $page;?></title>
+</head>
+<body>
+
+<!-- Navigation list shown at the top of the page -->
+<div id="nav">
+    <ul id="navlist">
+        <li onclick="location.href='index.php'">Home</li>
+        <li onclick="location.href='about.php'">About Us</li>
+        <li onclick="location.href='contact.php'">Contact Us</li>
+        <li onclick="location.href='curriculum.php'">Cirriculum</li>
+        <li onclick="location.href='media.php'">Pictures & Videos</li>
+        <li onclick="location.href='schedule.php'">Classes & Events</li>
+    </ul>
+</div><br>
