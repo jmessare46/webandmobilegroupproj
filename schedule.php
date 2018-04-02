96ae34b40b071aabcc496175aa32c90ba7ffe9d8
Animecrazyluv <36174233+Animecrazyluv@users.noreply.github.com>
2018-04-02 16:22:40 -0400

    Updated Schedule 1
    Needs php to request a private lesson.

---
+++ schedule.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Kung Fu site Schedule page.
+ *
+ * @version 4/2/18
+ * @author Rachael Simmonds
+ */
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Schedule</title>
+</head>
+<body>
+
+<!-- Navigation list shown at the top of the page -->
+<div id="nav">
+    <ul id="navlist">
+        <li><a href="index.php">Home</a></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li class="active"><a href="schedule.php">Schedule</a></li>
+    </ul>
+</div>
+
+<div id="classTimes">
+	<h1>Class Times</h1>
+			<br>
+			<br>
+		<h2>Monday & Wednesday: 6:30 - 8:00 PM</h2>
+			<br>
+			<br>
+		<h2>Thursday: 8:00 AM - 9:00 AM</h2>
+			<br>
+			<br>
+		<h2>Saturday: 11:00 AM - 12:30 PM</h2>
+			<br>
+			<br>
+	<h1>Private Lessons</h1>
+	<br>
+		<div id="pTag">
+			<p>For those who would like private lessons, </p>
+			<p>you are able to schedule a time below. </p>
+		</div>
+</div>
+
+
+
+
+
+
+
