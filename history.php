a680ef754f38f14883c7182e5ee58245034bcaa9
Joe <jmessare46@gmail.com>
2018-04-03 09:39:38 -0400

    Added lineage of school and styled the headings

--- history.php
+++ history.php
@@ -18,21 +18,73 @@ include $path . 'assets/inc/header.php';
 <p>
     <div id="timeline">
         <div class="left">
+            <h3>NG MUI</h3>
             <p>
                 Left history content
             </p>
         </div>
         <div class="right">
+            <h3>YIM VING TSUN</h3>
             <p>
                 Right history content
             </p>
         </div>
         <div class="left">
+            <h3>LEUNG POK TOA</h3>
             <p>
                 Left history content
             </p>
         </div>
         <div class="right">
+            <h3>WONG WAH BO</h3>
+            <p>
+                Right history content
+            </p>
+        </div>
+        <div class="left">
+            <h3>LEUNG LAN KWAI</h3>
+            <p>
+                Left history content
+            </p>
+        </div>
+        <div class="right">
+            <h3>LEUNG YEE TAI</h3>
+            <p>
+                Right history content
+            </p>
+        </div>
+        <div class="left">
+            <h3>DR. LEUNG JUN</h3>
+            <p>
+                Left history content
+            </p>
+        </div>
+        <div class="right">
+            <h3>CHAN WAH SHUN</h3>
+            <p>
+                Right history content
+            </p>
+        </div>
+        <div class="left">
+            <h3>LEUNG BIK</h3>
+            <p>
+                Left history content
+            </p>
+        </div>
+        <div class="right">
+            <h3>YIP MAN</h3>
+            <p>
+                Right history content
+            </p>
+        </div>
+        <div class="left">
+            <h3>MOY YAT</h3>
+            <p>
+                Left history content
+            </p>
+        </div>
+        <div class="right">
+            <h3>MASTER DON GREEN (MOY DON)</h3>
             <p>
                 Right history content
             </p>
