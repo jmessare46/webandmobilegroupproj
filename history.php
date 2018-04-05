40ddaadce9d889b64dfd42f20df090b4faf065b9
Joe <jmessare46@gmail.com>
2018-04-05 19:43:14 -0400

    Removed teachers from the timeline
    Owner said that it was uneccessary to have all of these people in here and said to just include the remaining important instructors

--- history.php
+++ history.php
@@ -30,63 +30,21 @@ include $path . 'assets/inc/header.php';
             </p>
         </div>
         <div class="left">
-            <h3>LEUNG POK TOA</h3>
-            <p>
-                Left history content
-            </p>
-        </div>
-        <div class="right">
-            <h3>WONG WAH BO</h3>
-            <p>
-                Right history content
-            </p>
-        </div>
-        <div class="left">
-            <h3>LEUNG LAN KWAI</h3>
-            <p>
-                Left history content
-            </p>
-        </div>
-        <div class="right">
-            <h3>LEUNG YEE TAI</h3>
-            <p>
-                Right history content
-            </p>
-        </div>
-        <div class="left">
-            <h3>DR. LEUNG JUN</h3>
-            <p>
-                Left history content
-            </p>
-        </div>
-        <div class="right">
-            <h3>CHAN WAH SHUN</h3>
-            <p>
-                Right history content
-            </p>
-        </div>
-        <div class="left">
-            <h3>LEUNG BIK</h3>
-            <p>
-                Left history content
-            </p>
-        </div>
-        <div class="right">
             <h3>YIP MAN</h3>
             <p>
-                Right history content
-            </p>
-        </div>
-        <div class="left">
-            <h3>MOY YAT</h3>
-            <p>
                 Left history content
             </p>
         </div>
         <div class="right">
-            <h3>MASTER DON GREEN (MOY DON)</h3>
+            <h3>MOY YAT</h3>
             <p>
                 Right history content
+            </p>
+        </div>
+        <div class="left">
+            <h3>MASTER DON GREEN (MOY DON)</h3>
+            <p>
+                Left history content
             </p>
         </div>
     </div>
