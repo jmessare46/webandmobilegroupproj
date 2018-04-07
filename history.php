6dc6b1bedc551a62d9e0cd284fbfdcfa230ad01a
Joe <jmessare46@gmail.com>
2018-04-07 19:31:40 -0400

    Began inserting the history given by the owner

--- history.php
+++ history.php
@@ -20,31 +20,70 @@ include $path . 'assets/inc/header.php';
         <div class="left">
             <h3>NG MUI</h3>
             <p>
-                Left history content
+                Ng Mui was a Buddhist involved with secret underground Kung Fu society. She was well known for her
+                outstanding fighting ability and her fused style. Although the origin of Ng Mui's style of martial arts
+                is unknown it was believed she used the both the snake and crane techniques in her unique fighting style
+                which at the time had no name. She later heard of a woman named Yim Ving Tsun and decided to begin
+                training her very first student...
             </p>
         </div>
         <div class="right">
             <h3>YIM VING TSUN</h3>
             <p>
-                Right history content
+                Yim Ving Tsun was a chinese woman who was promised to marry Leung Bok Choy. She however attracted the
+                unwanted attention of a chinese war lord who wanted her hand in marriage. She of course refused and so
+                in response the war lord began to harass and beat up Yim Ving Tsun's father and husband to be. Ng Mui
+                stepped in to the picture and told Yim Ving Tsun to challenge the warlord to a challenge match in 6
+                months on the premise that she couldn't marry someone she could best in combat. When the warlord heard
+                this offer he accepted thinking nothing of this small woman. Yim Ving Tsun trained for 6 months with Ng
+                Mui and when she returned she defeated the warlord who was never to be seen again. As Ng Mui continued
+                to train Yim Ving Tsun she realized that she needed to establish officially what she was teaching. She
+                named the art Ving Tsun which means everlasting spring time or new beginning in Yim Ving Tsun's honor.
+                The second student of this art was Yim Ving Tsun's husband Leung Bok Choy.
             </p>
         </div>
         <div class="left">
             <h3>YIP MAN</h3>
             <p>
-                Left history content
+                Yip Man was a famous teacher of Ving Tsun who revitalized the system. He was educated in short and long
+                style Ving Tsun which were taught by the two prominent Ving Tsun teachers at the time. He took their
+                styles and perfected it in to one. He also changed the style to adapt to the globalizing world of
+                martial arts. In 1949 when Yip Man moved from mainland China to Hong Kong he saw a multitude of
+                different styles of martial arts from around the world. In particular American and British boxers along
+                with Russian wrestlers were setting up challenge matches in Hong Kong to prove that their style of
+                martial arts was the best. Time and time again they were besting the local martial artists who had never
+                trained against these styles. Yip Man adapted Ving Tsun to account for these different styles and he
+                became famous due to his school's undefeated challenge match record and for the fact that Yip Man's
+                school was the only one who could stand up to these foreigners. Yip Man was also known as one of Bruce
+                Lee's prominent teachers starting his training with him in 1957. Despite this recognition Bruce Lee only
+                trained with Yip Man for a short period of time before going to America in 1959.
             </p>
         </div>
         <div class="right">
             <h3>MOY YAT</h3>
             <p>
-                Right history content
+                Moy Yat was known as the "librarian" of the Ving Tsun system. He earned this name because of his
+                construction (with the careful supervision of Yip Man) of the Kuen Kuit which was an inscription of all
+                of the teachings of Ving Tsun on jade rock in order to preserve these teachings. Moy Yat was given this
+                task because he was so well know for his calligraphy and his high level of education. Today the Kuen
+                Kuit is in the possession of Moy Yat's son William Moy. Moy Yat was also the youngest Sifu (teacher) to
+                teach in Yip Man's schools and the first master under Yip man to bring Ving Tsun to America.
             </p>
         </div>
         <div class="left">
             <h3>MASTER DON GREEN (MOY DON)</h3>
             <p>
-                Left history content
+                - Started wing Chung in Virginia under Duncan Leung and for the time he was in service
+                - 17-20 years old
+                - When he returned to Rochester NY started taking Ving Tsun from moy yat in NYC
+                - Because of the travel time to NYC moy yat said for him to train with one of his students in Canada
+                - Dunn Wah Tang was this teacher (Sunny Tang)
+                - When he Finished with Sunny Tang he was to come back to NYC and finish with moy yat
+                - Then became Sifu under moy yat
+                - Started school at Dunn Was Tang in Rochester at that point so the school was changed to be under the moy yat family school which included Sunny Tang
+                - Don green became a disciple under moy yat (Bi Si) in 1994
+                - Don’s School started in September 1992
+                - Was relocated and moved to current location
             </p>
         </div>
     </div>
