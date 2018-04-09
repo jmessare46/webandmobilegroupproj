89e25ee0995d72a3595a56265bd5d319aeae89aa
Joe <jmessare46@gmail.com>
2018-04-08 22:09:28 -0400

    Created a page to authenticate the users login information

--- loginAuth.php
+++ loginAuth.php
@@ -1,7 +1,44 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: josephmessare
+ * User: Joseph Messare
  * Date: 4/8/18
  * Time: 9:47 PM
  */
+
+
+session_start();
+
+$username = $_POST['uname'];
+$password = $_POST['psw'];
+
+// If login is clicked
+if (isset( $_POST['submit'] ))
+{
+    // If neither username or password is empty
+    if(empty( $username ) || empty( $password ))
+    {
+        echo "Username or Password error";
+    }
+    else
+    {
+        // Checks if password and user are correct
+        if( $username == "joe" && $password == "test" )
+        {
+            // User and password combo was correct and user is sent to members page
+            header("location: member.php");
+            $_SESSION['user'] = "joe";
+        }
+        else
+        {
+            // Redirect to login page
+            header("location: login.php");
+        }
+    }
+}
+else
+{
+    // Something went really wrong
+    echo "Page submission error";
+}
+    ?>
