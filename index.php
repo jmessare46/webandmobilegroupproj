072a5449ffd3c75a5e6b6068ad302a06b7c48457
Joe <jmessare46@gmail.com>
2018-04-11 17:15:43 -0400

    Started to use slide show on homepage

--- index.php
+++ index.php
@@ -13,8 +13,19 @@ include $path . 'assets/inc/header.php';
 
 <h1 class="title"><?php echo $page ?></h1>
 
-<p>
-    Page content would go here.
-</p>
+
+<!-- Slide show of Las Vegas -->
+<div id="slideshow">
+    <div id="slides">
+        <img src="assets/images/Las_Vegas.jpg" alt="Las Vegas" class="slides"/>
+
+        <img src="assets/images/paris_las_vegas.jpg" alt="Paris Las Vegas" class="slides"/>
+
+        <img src="assets/images/Nightclubs-and-Limo.jpg" alt="Night Club" class="slides"/>
+        <div id="nslide" onclick="nextSlide(1)">&#8594;</div>
+        <div id="bslide" onclick="nextSlide(-1)">&#8592;</div>
+    </div>
+</div><br>
+
 </body>
 </html>
