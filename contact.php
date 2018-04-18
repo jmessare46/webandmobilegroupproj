1bcc38aac874c8e08f2c31071c76c162f9ac1d39
kmj1556 <36195754+kmj1556@users.noreply.github.com>
2018-04-18 16:49:02 -0400

    some edits
    add a bit of css

--- contact.php
+++ contact.php
@@ -14,35 +14,43 @@ include $path . 'assets/inc/header.php';
 <h1 class="title"><?php echo $page ?></h1>
 
 <body>
-	
+	<!--mastergreen86@gmail.com real email-->
 	<p>
-		Phone: 123-456-7890
-	</p>
-	<p>
-		Email: moydonkungdfu@gmail.com
+		Phone: 123-456-7891
 	</p>
 	<a href="https://www.facebook.com/Moy-Don-Wing-Chun-Kung-Fu-1542972345772857/">Our Facebook Page </a>
 	
 	<h2>Ready to Apply?</h2>
 	<p>Contact Us Here For More Info</p>
-	<form action="assets/inc/process_gpProj.php" method="POST">
-		First Name: <br>
-		<input type="text" name="fname">
-		<br>
+	<form action="gpProj/process_gpProj.php" method="POST">
+		<div class="contactUs">
+		First Name:<br>
+		<input class="contactUs" type="text" name="fname">
+		</div>
+		
+		<div class="contactUs">
 		Last Name:<br>
-		<input type="text" name="lname">
-		<br>
+		<input class="contactUs" type="text" name="lname">
+		</div>
+		
+		<div class="contactUs">
 		Phone Number:<br>
-		<input type="text" name="pnumber">
-		<br>
+		<input class="contactUs" type="text" name="pnumber">
+		</div>
+		
+		<div class="contactUs">
 		Email:<br>
 		<input type="text" name="email">
-		<br>
+		</div>
+		
+		<div class="contactUs">
 		Description:<br>
-		<input type="text" name="desc">
-		<br>
-		<br>
+		<input id="desc" type="text" name="desc">
+		</div>
+		
+		<div class="contactUs">
 		<input type="submit" value="Submit">
+		</div>
 	</form>
 
 </body>
