64a1e7ad5b320540888fca6e38bb4d7ad90badc5
kmj1556 <36195754+kmj1556@users.noreply.github.com>
2018-04-19 16:54:17 -0400

    CSS for contact us
    Formatted contact us page and added a facebook logo

--- contact.php
+++ contact.php
@@ -11,47 +11,51 @@ $page = 'Contact Us';
 include $path . 'assets/inc/header.php';
 ?>
 
-<h1 class="title"><?php echo $page ?></h1>
 
 <body>
+<div id="contact">
+	<div id="contactTitle">
+	<a href="https://www.facebook.com/Moy-Don-Wing-Chun-Kung-Fu-1542972345772857/">
+	<img id="fb" src="assets/images/fbLogo.png" alt="Facebook Logo" title="Facebook Logo"></a>
+<h1 class="title"><?php echo $page ?></h1>
+
 	<!--mastergreen86@gmail.com real email-->
 	<p>
 		Phone: 123-456-7891
 	</p>
-	<a href="https://www.facebook.com/Moy-Don-Wing-Chun-Kung-Fu-1542972345772857/">Our Facebook Page </a>
-	
 	<h2>Ready to Apply?</h2>
-	<p>Contact Us Here For More Info</p>
+	</div>
 	<form action="gpProj/process_gpProj.php" method="POST">
 		<div class="contactUs">
-		First Name:<br>
-		<input class="contactUs" type="text" name="fname">
+		<div id="fname"><span class="asterick">*</span> First Name:</div>
+		<input class="text" type="text" name="fname">
 		</div>
 		
 		<div class="contactUs">
-		Last Name:<br>
-		<input class="contactUs" type="text" name="lname">
+		<div id="lname"><span class="asterick">*</span> Last Name:</div>
+		<input class="text" type="text" name="lname">
 		</div>
 		
 		<div class="contactUs">
-		Phone Number:<br>
-		<input class="contactUs" type="text" name="pnumber">
+		<div id="pnum">Phone Number:</div>
+		<input class="text" type="text" name="pnumber">
 		</div>
 		
 		<div class="contactUs">
-		Email:<br>
-		<input type="text" name="email">
+		<div id="eMail"><span class="asterick">*</span> Email:</div>
+		<input class="text" type="text" name="email">
 		</div>
 		
 		<div class="contactUs">
-		Description:<br>
+		<div id="txtDesc"><span class="asterick">*</span> Description:</div>
 		<input id="desc" type="text" name="desc">
 		</div>
 		
 		<div class="contactUs">
-		<input type="submit" value="Submit">
+		<input id="submit" type="submit" value="Send">
 		</div>
 	</form>
-
+</div>
 </body>
+
 </html>
