6479fdb4fdaaeb552586c3ca6aa087dd1b6a712e
Animecrazyluv <36174233+Animecrazyluv@users.noreply.github.com>
2018-04-24 10:31:52 -0400

    New changes to schedule/css

--- schedule.php
+++ schedule.php
@@ -23,10 +23,10 @@ include $path.'assets/inc/header.php';
 		  $res=$mysqli->query('SELECT day, className, beginHr, beginMin, timeOfDay1, endHr, endMin, timeOfDay2 FROM classTimes');
 		  if($res){
 			while($rowHolder = mysqli_fetch_array($res,MYSQLI_ASSOC)){
-				$records2[] = $rowHolder;
+				$records[] = $rowHolder;
 			}// end of while
 		  }// end of if
-			//var_dump($records2);
+			//var_dump($records);
 		}// end of if-mysqli
 		else {
 			echo "<h1> Error Connecting to Database</h1>";
@@ -37,17 +37,27 @@ include $path.'assets/inc/header.php';
 		<div class="times">
 
 			<?php
-					//var_dump($records1);
-					foreach($records1 as $this_row){
-						echo '<h2>'.$this_row['day'] . " " . $this_row['className']." " . $this_row['beginHr']."
-						". $this_row['beginMin']." " . $this_row['timeOfDay1']." " . $this_row['endHr']."
-						". $this_row['endMin']." ". $this_row['timeOfDay2'].'</h2>';
+					//var_dump($records);
+					foreach($records as $this_row){
+						echo '<h2>'.$this_row['day'] . " " . $this_row['className']." " . $this_row['beginHr'].
+						":". sprintf("%02d",$this_row['beginMin'])." " . $this_row['timeOfDay1']." " . $this_row['endHr'].
+						":". sprintf("%02d",$this_row['endMin'])." ". $this_row['timeOfDay2'].'</h2>';
 					}
 			?>
 
 			</div>
 		</div>
-</div>
+	<div class="con">
+		<p>If you would like to schedule a private lesson,</p>
+		<p>please click the button below.</p>
+		<ul>
+		<li class="button1"><span><a href="contact.php">Contact Us</a></span></li>
+		</ul>
+	</div>
+
+	<div class="events">
+
+	</div>
 
 
 		<hr>
