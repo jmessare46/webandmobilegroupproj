1d7c98faeb6f041bfcaaafabec62d0219ac51497
Joe <jmessare46@gmail.com>
2018-05-07 15:52:55 -0400

    Fixed issues on schedule page

--- schedule.php
+++ schedule.php
@@ -19,16 +19,16 @@ include $path.'assets/inc/header.php';
 		if ($mysqli) 
 		{
 		  // Gets contents of table and send back...
-		  $res=$mysqli->query('SELECT (day, start, end) FROM classTimes');
+		  $res=$mysqli->query('SELECT day, start, end FROM classTimes');
 		  $records = array();
-		  if($res){
+		  if($res)
+		  {
 			while($rowHolder = mysqli_fetch_array($res, MYSQLI_ASSOC))
 			{
 				$records[] = $rowHolder;
-			}// end of while
-		  }// end of if
-			//var_dump($records);
-		}// end of if-mysqli
+			}
+		  }
+		}
 		else 
 		{
 			echo "<h1> Error Connecting to Database</h1>";
@@ -37,37 +37,29 @@ include $path.'assets/inc/header.php';
 
 	?>
 		<div class="times">
-
 			<?php
 				// var_dump($records);
+                echo "<table style='float: left;'><tr><th>Day</th><th>Start Time</th><th>End Time</th></tr>";
 				foreach($records as $this_row)
 				{
-					echo"<div style='text-align:center; border-style: dotted; background-color:#e6ffe6; font-size:large;'> " .
-					"<tr>
-				      <td style='border: none; background-color: #dddddd; padding: 5px; width: 100px;'> " . $this_row['day'] . "</td>
-                      <td style='border: none; background-color: #dddddd; padding: 5px; width: 100px;'>" . $this_row['start'] . "</td>
-                      <td style='border: none; background-color: #dddddd; padding: 5px; width: 100px;'>" . $this_row['end'] .  "</td>
-					  </tr>";   
-					// echo '<h2>' . $this_row['day'] . " " . $this_row['start'] . " " . $this_row['end'] . '</h2>';
+					echo "<tr><td> " . $this_row['day'] . "</td><td>" . $this_row['start'] . "</td><td>" . $this_row['end'] . "</td></tr>";
 				}
+                echo "</table>";
 			?>
+            <div class="button1">
+                <span><a style="float: right;" href="contact.php">Contact Us</a></span>
+            </div>
 
-			</div>
-		</div>
-	<div class="con">
-		<p>If you would like to schedule a private lesson,</p>
-		<p>please click the button below.</p>
+            <div class="con">
+                <p>If you would like to schedule a private lesson,</p>
+                <p>please click the button below.</p>
 
-	</div>
-	<div class="button1">
-				<span><a href="contact.php">Contact Us</a></span>
-	</div>
+            </div>
 
-	<div class="events">
-		<center><iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQyeAArOAxuCmY64HjBmmBney6cxg1RiNaLnkB0UX15WYomLVMa8jLEoSZ5YF86e_U3GWNpzdQkx2xD/embed?start=true&loop=true&delayms=10000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe> </center>
-	</div>
-
-
-
-	</body>
+            <div class="events">
+                <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQyeAArOAxuCmY64HjBmmBney6cxg1RiNaLnkB0UX15WYomLVMa8jLEoSZ5YF86e_U3GWNpzdQkx2xD/embed?start=true&loop=true&delayms=10000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+            </div>
+        </div>
+</div>
+</body>
 </html>
