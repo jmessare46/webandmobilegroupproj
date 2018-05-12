96a214938cafd7fdbc86ca7b300ae052893e83bc
Joe <jmessare46@gmail.com>
2018-05-12 13:24:02 -0400

    Started fixing some of the issues with about page

--- about.php
+++ about.php
@@ -54,48 +54,61 @@ include $path.'assets/inc/header.php';
          <p>
              Master Donald Green (aka Moy Don) is a Master at Wing Chun (Ving Tsun) Kung-Fu. He started his journey in the
              Martial Arts at the age of 6 years old; for three years, he learned several Southern Chinese styles, mainly Southern
-             Shaolin Long Fist and Chen Style Tai Chi. </p>
-
-	<br>
- 	 <p> At 9, under the tutelage of Master Ting, he learned Tiger Claw Style as well as a
+             Shaolin Long Fist and Chen Style Tai Chi.
+         </p>
+         <br>
+ 	     <p>
+             At 9, under the tutelage of Master Ting, he learned Tiger Claw Style as well as a
              closed door family system similar to Black Tiger Claw until he left for the Navy at age 17. Also, during that same time, he
              would train in several other forms of combat. He trained in Tiger Claw on the weekends and other forms of Martial
-             Arts such as Chito Ryu Karate, Ju Jit Su, Judo, Greco Roman and Free Style Wrestling, Boxing, and Kick Boxing on other days.</p>
-	<br>
-	 <p>
+             Arts such as Chito Ryu Karate, Ju Jit Su, Judo, Greco Roman and Free Style Wrestling, Boxing, and Kick Boxing on other days.
+         </p>
+         <br>
+         <p>
              During this time, Master Green competed in many point tournaments. He won several tournaments and also started doing full
              contact no rules fighting. He took several seminars in Wing Chun, but could not find a teacher (Sifu) close
-             enough to take lessons from. </p>
-	<br>
-	 <p> When Master Green arrived in his first duty station in the Navy, he was able to take
+             enough to take lessons from.
+         </p>
+         <br>
+         <p>
+             When Master Green arrived in his first duty station in the Navy, he was able to take
              formal lessons from Master Duncan Leung in Virginia Beach, Virginia along with a friend from his team, Hector Cortez.
              They learned Wing Chun together and trained while deployed. Green also trained in Kajukembo, in which Hector was a
              Black Belt. During those Navy years, Green was also able to train in Mongolian Wrestling in Italy, Muay Thai in
-             Thailand, and Krav Maga in Israel.</p>
-	<br>
-	 <p> After leaving the military, he returned to Rochester, New York in search of a life
+             Thailand, and Krav Maga in Israel.
+         </p>
+         <br>
+         <p>
+             After leaving the military, he returned to Rochester, New York in search of a life
              long teacher to finish his training in Wing Chun, the Martial Art that would finally take his full interest and
              loyalty. He took a job teaching in a local Wing Chun club but traveled to New York City to start training with Grand
              Master Moy Yat. During this time, he would travel to the city on weekends to train and work in security and body
-             guarding during the week. He worked with clients such as Lou Gramm and “Down Town” Julie Brown.</p>
-	<br>
-	 <p> This took its
+             guarding during the week. He worked with clients such as Lou Gramm and “Down Town” Julie Brown.
+         </p>
+         <br>
+         <p>
+             This took its
              toll on Master Green. He took Grandmaster Moy Yat’s advice and trained with Moy's student -now Grandmaster- Dunn
              Wah (Sunny) Tang in Toronto, Canada. Green earned his Gold Sash, or mastery of the system, under Grandmaster Tang in
-             1993. </p>
-	<br>
-	 <p> He also competed for Team Canada and continued fighting all over the world in the Canadian Championships,
+             1993.
+         </p>
+         <br>
+         <p>
+             He also competed for Team Canada and continued fighting all over the world in the Canadian Championships,
              European Championships, and many other five-star ranked tournaments worldwide, including three full contact Muay Thai
              matches in Thailand, winning all three. Green then returned to Grandmaster Moy Yat and became his disciple in a Bise
-             ceremony.</p>
-	<br>
-	 <p> He stayed with Grandmaster Moy Yat until Moy's death, and achieved mastery of the system through him. Green continued to compete in 
-		full contact tournaments worldwide, winning world
-             tournaments and competing for both Canada and America before retiring from fighting when his first son was born
+             ceremony.
+         </p>
+         <br>
+         <p>
+             He stayed with Grandmaster Moy Yat until Moy's death, and achieved mastery of the system through him. Green continued to compete in
+             full contact tournaments worldwide, winning world tournaments and competing for both Canada and America before retiring from fighting when his first son was born
              at the age of 34. He is also a veteran of several real life challenge matches witnessed by students and friends
-             alike. Green also continued to work in Bouncing, Club Security, and private bodyguarding. </p>
-	<br>
-	<p>  Green currently lives in Webster,
+             alike. Green also continued to work in Bouncing, Club Security, and private bodyguarding.
+         </p>
+         <br>
+         <p>
+             Green currently lives in Webster,
              New York with his wife Jennifer, his three sons, and his two step daughters. Green is now celebrating his 43rd
              year in the Martial Arts and his 32nd year exclusively in Wing Chun Kung Fu. He has been included in the Action
              Martial Arts Hall of Fame and received several rewards for teaching Martial Arts. He has been teaching in
@@ -106,73 +119,83 @@ include $path.'assets/inc/header.php';
 		  <div class="instructor1">
 		   <h3>Adam Dunn - Assistant Instructor</h3><br>
 		 <img src="assets/images/InstructorAdam.jpg" alt="Sifu Dunn" class="insPic"/><br><br>
-		 <p class = "insBio">Adam first became involved with Wing Chun in 1998. His lack of prior experience led to rather superficial desires and expectations from the art. It soon became obvious that Wing Chun was not what he had been seeking, but the efficiency, efficacy, and logic of the art resonated so strongly with him that he never
-         left.</p><br>
-	<p>Despite a two year hiatus, he began acting as a junior instructor in 2003. Sifu Green’s only Bai Si
-          ceremony to date was performed in 2005, and it was at this time that Adam became a disciple. He was
-          extremely proud to be awarded his gold sash and the title of Sifu in 2011. Adam continues to act in the
-          role of senior instructor until such time as he chooses to take on students of his own. </p>
+		 <p class = "insBio">
+             Adam first became involved with Wing Chun in 1998. His lack of prior experience led to rather superficial
+             desires and expectations from the art. It soon became obvious that Wing Chun was not what he had been
+             seeking, but the efficiency, efficacy, and logic of the art resonated so strongly with him that he never
+             left.
+         </p><br>
+              <p>
+                Despite a two year hiatus, he began acting as a junior instructor in 2003. Sifu Green's only Bai Si
+                ceremony to date was performed in 2005, and it was at this time that Adam became a disciple. He was
+                extremely proud to be awarded his gold sash and the title of Sifu in 2011. Adam continues to act in the
+                role of senior instructor until such time as he chooses to take on students of his own.
+              </p>
 		 </div><br><br><br>
 		  <hr><br><div class="instructor2">
 			<h3>Sifu Nathan Melenbacker - Assistant Instructor</h3><br>
 				 <img src="assets/images/natemelenbacker.jpeg" class ="insPic" alt="Sifu Melenbacker"/><br><br>
-				 <p class = "insBio">Nate has over 30 years of experience in the martial arts, starting with Karate styles (Isshin-ryu, Kobudo, Kyokushin) and switching to Ving Tsun after meeting Sifu Don Green in 1992.  An Intelligence Specialist and Gulf War Navy Veteran, Nate achieved Gold Sash level along with his training partner, Sifu Adam Dunn, in 2011.</p>
+				 <p class = "insBio">
+                     Nate has over 30 years of experience in the martial arts, starting with Karate styles (Isshin-ryu,
+                     Kobudo, Kyokushin) and switching to Ving Tsun after meeting Sifu Don Green in 1992.  An
+                     Intelligence Specialist and Gulf War Navy Veteran, Nate achieved Gold Sash level along with his
+                     training partner, Sifu Adam Dunn, in 2011.
+                 </p>
 			  </div>
-		</div><br><br><br>
+     </div><br><br>
+ </div><br>
+
 		
-   </div><br><br><br><br><br>	
-		
-          <div class="container">
+   </div><br><br><br><br><br>
+    <div class="container">
 
 	<header id="FAQheader">
 		<h1>FAQ</h1>
 	</header>
-  
-<div class="downloads">
+        <div class="downloads">
   <ul>
   <h1>Downloads</h1><br>
-    <li> <a href="assets/images/Rules_for_Junior_Members.pdf" download="Rules For Junior Members (K-12)">Rules for Junior Members K - 12</a><br></li><br>
+    <li><a href="assets/images/Rules_for_Junior_Members.pdf" download="Rules For Junior Members (K-12)">Rules for Junior Members K - 12</a><br></li><br>
     <li><a href="assets/images/Rules_of_the_Kwoon.pdf" download="Rules for the Kwoon">Rules for the Kwoon (School)</a><br></a></li><br>
     <li><a href="assets/images/Statement%20of%20Commitment.jpeg" download="Statement of Commitment">Statement of Commitment</a></li><br>
   </ul>
 </div>
 
-<article>
-  <h1>Answers Straight from Master Green</h1><br>
-  
-           <h3>Why invest in Wing Chun?</h3>
-         <audio controls>
-             <source src="assets/audio/costsofwingchun.m4a" type="audio/mp4">
-             Your browser does not support the audio element.
-         </audio><br>
+    <article>
+    <h1>Answers Straight from Master Green</h1><br>
 
-         <h3>Why pick Wing Chun?</h3>
-         <audio controls>
-             <source src="assets/audio/whywingchun.m4a" type="audio/mp4">
-             Your browser does not support the audio element.
-         </audio><br>
+        <h3>Why invest in Wing Chun?</h3>
+        <audio controls>
+            <source src="assets/audio/costsofwingchun.m4a" type="audio/mp4">
+            Your browser does not support the audio element.
+        </audio><br>
 
-         <h3>Is or can Wing Chun be used for sport?</h3>
-         <audio controls>
-             <source src="assets/audio/forsport.m4a" type="audio/mp4">
-             Your browser does not support the audio element.
-         </audio><br>
+        <h3>Why pick Wing Chun?</h3>
+        <audio controls>
+            <source src="assets/audio/whywingchun.m4a" type="audio/mp4">
+            Your browser does not support the audio element.
+        </audio><br>
 
-         <h3>Does Wing Chun have ground fighting?</h3>
-         <audio controls>
-             <source src="assets/audio/groundfighting.m4a" type="audio/mp4">
-             Your browser does not support the audio element.
-         </audio><br>
+        <h3>Is or can Wing Chun be used for sport?</h3>
+        <audio controls>
+            <source src="assets/audio/forsport.m4a" type="audio/mp4">
+            Your browser does not support the audio element.
+        </audio><br>
 
-         <h3>Why train martial arts / Wing Chun for an extended period of time?</h3>
-         <audio controls>
-             <source src="assets/audio/whytrainsolong.m4a" type="audio/mp4">
-             Your browser does not support the audio element.
-         </audio><br>
-    
-</article>
+        <h3>Does Wing Chun have ground fighting?</h3>
+        <audio controls>
+            <source src="assets/audio/groundfighting.m4a" type="audio/mp4">
+            Your browser does not support the audio element.
+        </audio><br>
 
-</div>
+        <h3>Why train martial arts / Wing Chun for an extended period of time?</h3>
+        <audio controls>
+            <source src="assets/audio/whytrainsolong.m4a" type="audio/mp4">
+            Your browser does not support the audio element.
+        </audio><br>
+
+    </article>
+    </div>
 </section>
 </body>
 </html>
